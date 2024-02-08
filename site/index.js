cd79470b2d103dfc6e73d58f5ea9ed4a8b591742
melloCoding <mrjeffreymello@gmail.com>
2024-02-08 09:21:01 -0500

    modified:   site/clothes_devlog.html 	modified:   site/index.css 	modified:   site/index.js

--- site/index.js
+++ site/index.js
@@ -1,4 +1,19 @@
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
+var element = document.getElementsByName("navbar");
 
-console.log(windowWidth)
+console.log(windowWidth);
+
+setInterval(function() {
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    if(windowWidth <= 619){
+        console.log("mobile")
+        element.classList.replace("pcnav", "mobilenav");
+    }
+    console.log("Finished with loop")
+}, 1000);
+
+if(windowWidth <= 619){
+    console.log("mobile")
+}
