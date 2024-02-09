bb9bcaf9bc0635e848bce749a79dbe8e24b7722c
melloCoding <mrjeffreymello@gmail.com>
2024-02-08 20:58:38 -0500

    modified:   site/clothes_devlog.html 	modified:   site/index.css 	modified:   site/index.js

--- site/index.js
+++ site/index.js
@@ -1,19 +1,34 @@
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
-var element = document.getElementsByName("navbar");
+//var element = document.getElementsByName("navbar");
 
-console.log(windowWidth);
 
 setInterval(function() {
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
-    if(windowWidth <= 619){
-        console.log("mobile")
-        element.classList.replace("pcnav", "mobilenav");
+    if(windowWidth > 619){
+        document.getElementById("navbar-mobile").style.display = "none";
+        document.getElementById("navbar-pc").style.display = "block";
+    }
+    else if(windowWidth <= 619){
+        document.getElementById("navbar-mobile").style.display = "block";
+        document.getElementById("navbar-pc").style.display = "none";
     }
     console.log("Finished with loop")
-}, 1000);
+}, 100);
 
-if(windowWidth <= 619){
-    console.log("mobile")
-}
+/*
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+  */
