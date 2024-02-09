f965f30e9b860d5975d68ca4a6bc8cff4c01a30b
melloCoding <mrjeffreymello@gmail.com>
2024-02-09 13:41:31 -0500

    modified:   site/clothes_devlog.html 	modified:   site/devlog.css 	modified:   site/devlog.html 	modified:   site/index.css 	modified:   site/index.html 	modified:   site/index.js 	modified:   site/projects.html 	modified:   site/robotics.html

--- site/index.js
+++ site/index.js
@@ -9,11 +9,13 @@ setInterval(function() {
     if(windowWidth > 619){
         document.getElementById("navbar-mobile").style.display = "none";
         document.getElementById("navbar-pc").style.display = "block";
+        document.getElementById("body").style.marginTop = "43px";
     }
     else if(windowWidth <= 619){
         document.getElementById("navbar-mobile").style.display = "block";
         document.getElementById("navbar-pc").style.display = "none";
-        document.getElementById("")
+        document.getElementById("body").style.marginBottom = "43px";
+        document.getElementById("body").style.marginTop = "0px";
     }
     
 }, 100);
