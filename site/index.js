ca4e117a77b8d1bf2fea401c224c7fc5bac1f9ec
melloCoding <mrjeffreymello@gmail.com>
2024-02-10 13:23:25 -0500

    modified:   site/clothes_devlog.html 	modified:   site/contact.html 	modified:   site/index.html 	modified:   site/index.js 	modified:   site/projects.html 	modified:   site/robotics.html

--- site/index.js
+++ site/index.js
@@ -9,13 +9,20 @@ setInterval(function() {
     if(windowWidth > 619){
         document.getElementById("navbar-mobile").style.display = "none";
         document.getElementById("navbar-pc").style.display = "block";
+        //change the margin from the bottom to the top so that the text doesnt go under the navbar
         document.getElementById("body").style.marginTop = "43px";
+        document.getElementById("body").style.marginBottom = "0px";
+        //
+        document.getElementById("game_promo").width = "552";
     }
     else if(windowWidth <= 619){
         document.getElementById("navbar-mobile").style.display = "block";
         document.getElementById("navbar-pc").style.display = "none";
-        document.getElementById("body").style.marginBottom = "43px";
+        //change the margin from the top to the bottom so that the text doesnt go under the navbar
         document.getElementById("body").style.marginTop = "0px";
+        document.getElementById("body").style.marginBottom = "43px";
+        //
+        document.getElementById("game_promo").width = "400";
     }
     
-}, 100);
+}, 1);
